e7813cad7d100ff9ccfc5d92625838ab86dec2a3
Andy Yu <yuandy1031@gmail.com>
2023-06-22 23:05:28 -0400

    Add buttons for different settings pages

--- app/src/main/java/com/example/memorylane/SettingsActivity.kt
+++ app/src/main/java/com/example/memorylane/SettingsActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.memorylane.ui.theme.MemorylaneTheme
 
 class SettingsActivity : ComponentActivity() {
@@ -33,11 +40,28 @@ fun SettingsPage(modifier: Modifier = Modifier) {
         modifier = modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Column() {
-            Text(text = "Settings")
-            Text(text = "Account")
-            Text(text = "Notifications")
-            Text(text = "Logout")
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Settings", Modifier.padding(vertical=40.dp))
+
+            Button(onClick = {}) {
+                Icon(imageVector = Icons.Outlined.Person, contentDescription = "Person Icon")
+
+                Text(text = "Account",Modifier.padding(start = 10.dp))
+            }
+
+            Button(onClick = {}) {
+                Icon(imageVector = Icons.Outlined.Notifications, contentDescription = "Notifications Icon")
+
+                Text(text = "Notifications",Modifier.padding(start = 10.dp))
+            }
+
+            Button(onClick = {}) {
+                Icon(imageVector = Icons.Outlined.Close, contentDescription = "Close Icon")
+
+                Text(text = "Logout",Modifier.padding(start = 10.dp))
+            }
         }
     }
 }
