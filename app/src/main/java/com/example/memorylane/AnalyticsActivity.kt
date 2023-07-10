067de7c96977ddda921f2092e030a80fb06ea241
ianeen <32077002+ianeen@users.noreply.github.com>
2023-07-10 14:12:44 -0400

    weekly analytics view updated to include weekly commonalities

--- app/src/main/java/com/example/memorylane/AnalyticsActivity.kt
+++ app/src/main/java/com/example/memorylane/AnalyticsActivity.kt
@@ -1,16 +1,23 @@
 package com.example.memorylane
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.memorylane.data.JournalEntryDO
+import com.example.memorylane.data.WeeklyStorageDO
 import com.patrykandpatrick.vico.compose.axis.horizontal.bottomAxis
 import com.patrykandpatrick.vico.compose.axis.vertical.startAxis
 import com.patrykandpatrick.vico.compose.chart.Chart
@@ -18,7 +25,6 @@ import com.patrykandpatrick.vico.compose.chart.column.columnChart
 import com.patrykandpatrick.vico.compose.chart.line.lineChart
 import com.patrykandpatrick.vico.core.entry.ChartEntryModelProducer
 import com.patrykandpatrick.vico.core.entry.FloatEntry
-import com.patrykandpatrick.vico.core.entry.entriesOf
 import io.realm.kotlin.Realm
 import io.realm.kotlin.RealmConfiguration
 import io.realm.kotlin.ext.query
@@ -30,22 +36,32 @@ val chartEntryModelProducer2 = ChartEntryModelProducer(getWeekHappiness())
 
 @Composable
 fun AnalyticsPage() {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "Week Word Count", textAlign = TextAlign.Center, color = MaterialTheme.colors.secondary, modifier = Modifier.padding(top = 16.dp))
+    val weeks = getWeeklyAllEntries()
+    var selectedWeek by remember { mutableStateOf(weeks.firstOrNull()) }
+
+    val positives = getWeeklyPositives(selectedWeek)
+    val negatives = getWeeklyNegatives(selectedWeek)
+    val workOns = getWeeklyWorkOns(selectedWeek)
+
+    Column(modifier = Modifier.verticalScroll(rememberScrollState()).padding(bottom = 64.dp)) {
+        Text(text = "Week Character Count", textAlign = TextAlign.Center, color = MaterialTheme.colors.secondary, modifier = Modifier.padding(top = 16.dp).fillMaxWidth())
         Chart(
             chart = lineChart(),
             chartModelProducer = chartEntryModelProducer1,
             startAxis = startAxis(),
             bottomAxis = bottomAxis(),
         )
-        Text(text = "Week Happiness", textAlign = TextAlign.Center, color = MaterialTheme.colors.secondary, modifier = Modifier.padding(top = 16.dp))
+        Text(text = "Week Happiness", textAlign = TextAlign.Center, color = MaterialTheme.colors.secondary, modifier = Modifier.padding(top = 16.dp).fillMaxWidth())
         Chart(
             chart = columnChart(),
             chartModelProducer = chartEntryModelProducer2,
             startAxis = startAxis(),
             bottomAxis = bottomAxis(),
         )
-        Text(text = "Frequently Recognized Problems", textAlign = TextAlign.Center, color = MaterialTheme.colors.secondary, modifier = Modifier.padding(top = 16.dp))
+        Text(text = "Recognized Entry Commonalities", textAlign = TextAlign.Center, color = MaterialTheme.colors.secondary, modifier = Modifier.padding(top = 16.dp).fillMaxWidth())
+        ListDisplay(title = "Overlap Positives", items = positives)
+        ListDisplay(title = "Overlap Issues", items = negatives)
+        ListDisplay(title = "Overlap Areas To Work On", items = workOns)
     }
 }
 
@@ -75,4 +91,23 @@ fun getWeekHappiness(): List<FloatEntry> = List(1) {
             FloatEntry(index.toFloat()+1, it.happinessRating)
         }
     }
+}
+
+fun getWeeklyAllEntries(): List<WeeklyStorageDO> {
+    val config = RealmConfiguration.create(schema = setOf(WeeklyStorageDO::class))
+    val realm = Realm.open(config)
+    val items: RealmResults<WeeklyStorageDO> = realm.query<WeeklyStorageDO>().sort("weekStartDate", Sort.DESCENDING).find()
+    return items.toList()
+}
+
+fun getWeeklyPositives(entry: WeeklyStorageDO?): List<String> {
+    return entry?.continuingPositives?.toList() ?: emptyList()
+}
+
+fun getWeeklyNegatives(entry: WeeklyStorageDO?): List<String> {
+    return entry?.problems?.toList() ?: emptyList()
+}
+
+fun getWeeklyWorkOns(entry: WeeklyStorageDO?): List<String> {
+    return entry?.thingsToWorkOn?.toList() ?: emptyList()
 }
