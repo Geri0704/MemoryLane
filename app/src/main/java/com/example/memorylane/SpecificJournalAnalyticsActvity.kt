eead9e59ead5f66ab174360ae13d049a9436b099
ianeen <32077002+ianeen@users.noreply.github.com>
2023-07-13 20:56:50 -0400

    emergency contact component implemented

--- app/src/main/java/com/example/memorylane/SpecificJournalAnalyticsActvity.kt
+++ app/src/main/java/com/example/memorylane/SpecificJournalAnalyticsActvity.kt
@@ -4,6 +4,8 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -16,7 +18,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withAnnotation
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import com.example.memorylane.data.JournalEntryDO
 import io.realm.kotlin.Realm
@@ -40,7 +50,9 @@ fun AnalyticsPageParent() {
         colors = if (MaterialTheme.colors.isLight) lightColors() else darkColors(),
     ) {
         Column {
-            Row(modifier = Modifier.fillMaxWidth().padding(16.dp)) {
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)) {
                 Button(
                     onClick = { showWeeklyAnalytics = false },
                     colors = ButtonDefaults.buttonColors(backgroundColor = if (showWeeklyAnalytics) Color.Gray else MaterialTheme.colors.primary),
@@ -70,7 +82,10 @@ fun AnalyticsPageParent() {
 @Composable
 fun SpecificAnalyticsPage(entry: String, positives: List<String>, negatives: List<String>, workOn: List<String>) {
     Column(
-        modifier = Modifier.padding(16.dp).verticalScroll(rememberScrollState()).padding(bottom = 64.dp)
+        modifier = Modifier
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState())
+            .padding(bottom = 64.dp)
     ) {
         Text(
             text = "Journal Entry",
@@ -96,6 +111,65 @@ fun SpecificAnalyticsPage(entry: String, positives: List<String>, negatives: Lis
         Spacer(modifier = Modifier.height(16.dp))
 
         ListDisplay(title = "Things to work on", items = workOn)
+
+        EmergencyContact()
+    }
+}
+
+@OptIn(ExperimentalTextApi::class)
+@Composable
+fun EmergencyContact() {
+    var emergencyContactVisible by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(
+            onClick = { emergencyContactVisible = !emergencyContactVisible },
+            modifier = Modifier.padding(top = 16.dp),
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.Transparent,
+                contentColor = Color.Black
+            ),
+            shape = RoundedCornerShape(50)
+        ) {
+            Text("Emergency Contact")
+        }
+    }
+
+    if (emergencyContactVisible) {
+        Text(
+            text = "Help Contact Information",
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .fillMaxWidth()
+        )
+        Text(
+            text = "Phone: 1-888-668-6810",
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .fillMaxWidth()
+        )
+        val uriHandler = LocalUriHandler.current
+        val annotatedLinkString = buildAnnotatedString {
+            withStyle(style = SpanStyle(textDecoration = TextDecoration.Underline)) {
+                withAnnotation(tag = "", annotation = "https://www.canada.ca/en/public-health/services/mental-health-services/mental-health-get-help.html") {
+                    append("https://www.canada.ca/en/public-health/services/mental-health-services/mental-health-get-help.html")
+                }
+            }
+        }
+        ClickableText(
+            text = annotatedLinkString,
+            onClick = { offset ->
+                annotatedLinkString.getStringAnnotations("URL", offset, offset).firstOrNull()?.let { annotation ->
+                    uriHandler.openUri(annotation.item)
+                }
+            },
+        )
     }
 }
 
@@ -145,7 +219,9 @@ fun DropdownMenu(entries: List<JournalEntryDO>, selectedEntry: JournalEntryDO, o
                     Icon(Icons.Filled.ArrowDropDown, contentDescription = "Dropdown icon")
                 }
             },
-            modifier = Modifier.fillMaxWidth().clickable { expanded = true }
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { expanded = true }
         )
 
         DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
