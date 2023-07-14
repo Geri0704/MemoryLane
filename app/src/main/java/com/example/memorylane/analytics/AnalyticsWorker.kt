7bfa79197cb2dc086cc336f532db0680579b6493
ianeen <32077002+ianeen@users.noreply.github.com>
2023-07-14 18:17:23 -0400

    separate weekly analytics to own worker so it can be run at different interval

--- app/src/main/java/com/example/memorylane/analytics/AnalyticsWorker.kt
+++ app/src/main/java/com/example/memorylane/analytics/AnalyticsWorker.kt
@@ -24,13 +24,6 @@ class AnalyticsWorker(context: Context, workerParameters: WorkerParameters) : Wo
         try {
             val gptRequest = WorkerAIClient()
 
-            try {
-                processWeeklyEntries()
-            } catch (e: Exception) {
-                println(e)
-                return Result.failure(workDataOf("Failed" to "Weekly analytics process failed"))
-            }
-
             val config = RealmConfiguration.create(schema = setOf(JournalEntryDO::class))
             val realm = Realm.open(config)
 
@@ -65,69 +58,5 @@ class AnalyticsWorker(context: Context, workerParameters: WorkerParameters) : Wo
             println(e)
             return Result.failure(workDataOf("Failed" to "Analytics process failed"))
         }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun processWeeklyEntries() {
-        val config = RealmConfiguration.create(schema = setOf(JournalEntryDO::class, WeeklyStorageDO::class))
-        val realm = Realm.open(config)
-
-        val currentDate = LocalDate.now()
-        val startDate = currentDate.minusDays(currentDate.dayOfWeek.value.toLong())
-        val endDate = startDate.plusDays(6)
-
-        val weekStartDateString = startDate.format(DateTimeFormatter.ofPattern("dd/MM"))
-        val weekEndDateString = endDate.format(DateTimeFormatter.ofPattern("dd/MM"))
-
-        println(weekStartDateString)
-        println(weekEndDateString)
-
-        val items: RealmResults<JournalEntryDO> = realm.query<JournalEntryDO>()
-            .sort("date", Sort.DESCENDING)
-            .find()
-
-        if (items.isEmpty()) return
-
-        val items2: RealmResults<WeeklyStorageDO> = realm.query<WeeklyStorageDO>("weekStartDate == '$startDate'")
-            .find()
-
-        if (items2.isNotEmpty()) return
-
-        val gptRequest = WorkerAIClient()
-        val weeklyStorage = WeeklyStorageDO().apply {
-            weekStartDate = startDate.toString()
-            weekEndDate = endDate.toString()
-        }
-
-        var prompt = "The following are numerous journal entries, return a list of the recognized positives, problems, and things to work on that they have in common if any: "
-
-        for ((index, entry) in items.withIndex()) {
-            prompt += "${index + 1}. ${entry.entry} "
-        }
-
-        val response = gptRequest.makeWeekGptRequest(prompt)
-
-        println(response)
-
-        val positives = if (response.first.isNotEmpty()) response.first.map { it.capitalize() } else listOf("None recognized")
-        val negatives = if (response.second.isNotEmpty()) response.second.map { it.capitalize() } else listOf("None recognized")
-        val workOns = if (response.third.isNotEmpty()) response.third.map { it.capitalize() } else listOf("None recognized")
-
-        weeklyStorage.continuingPositives.addAll(positives)
-        weeklyStorage.problems.addAll(negatives)
-        weeklyStorage.thingsToWorkOn.addAll(workOns)
-
-        realm.writeBlocking {
-            copyToRealm(WeeklyStorageDO().apply {
-                id = UUID.randomUUID().toString()
-                weekStartDate = weeklyStorage.weekStartDate
-                weekEndDate = weeklyStorage.weekEndDate
-                continuingPositives.addAll(weeklyStorage.continuingPositives)
-                problems.addAll(weeklyStorage.problems)
-                thingsToWorkOn.addAll(weeklyStorage.thingsToWorkOn)
-            })
-        }
-
-        realm.close()
     }
 }
