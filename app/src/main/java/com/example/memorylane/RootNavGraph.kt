5967447fc963a94e92bf557122ce2c94842af9d3
Geri <geri@korio.ca>
2023-07-15 00:02:07 -0400

    Root navigation now checks if we have an authToken cached and correctly routes to correct page

--- app/src/main/java/com/example/memorylane/RootNavGraph.kt
+++ app/src/main/java/com/example/memorylane/RootNavGraph.kt
@@ -1,18 +1,24 @@
 package com.example.memorylane
 
+import android.content.Context
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 
 @Composable
 fun RootNavigationGraph(navController: NavHostController){
+    val context = LocalContext.current
+    val sharedPref = context.getSharedPreferences("UserSettings", Context.MODE_PRIVATE)
+    val authToken = sharedPref.getString("authToken", null)
+
     NavHost(
         navController = navController,
         route = Graph.ROOT,
-        startDestination = Graph.HOME
+        startDestination = if (authToken == null ) Graph.AUTHENTICATION else Graph.HOME
     ){
-        authNavGraph(navController = navController)
+        authNavGraph(navController = navController, context)
         composable(route = Graph.HOME){
             mainScreen()
         }
