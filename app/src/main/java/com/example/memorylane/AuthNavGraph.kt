92d580fe90157d20712bf860cf764fbce3120c3b
Geri <geri@korio.ca>
2023-07-15 00:03:56 -0400

    change authNavGraph to recieve app context as parameter so the same state can be based to login and account creation

--- app/src/main/java/com/example/memorylane/AuthNavGraph.kt
+++ app/src/main/java/com/example/memorylane/AuthNavGraph.kt
@@ -2,40 +2,39 @@ package com.example.memorylane
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 
-fun NavGraphBuilder.authNavGraph(navController: NavController){
+fun NavGraphBuilder.authNavGraph(navController: NavController, context: android.content.Context){
     navigation(
         route = Graph.AUTHENTICATION,
         startDestination = AuthScreen.Login.route
     ){
          composable(route = AuthScreen.Login.route) {
-             var authToken = remember{ mutableStateOf("") }
              LogInPage(
-                 token = authToken,
                  onLoginSuccessful = {
                      navController.popBackStack()
                      navController.navigate(Graph.HOME)
                  },
                  onCreateAccountClick = {
                      navController.navigate(AuthScreen.CreateAccount.route)
-                 }
+                 },
+                 context = context
              )
          }
         composable(route = AuthScreen.CreateAccount.route){
-            var authToken = remember{ mutableStateOf("") }
             AccountCreation(
-                token = authToken,
                 onCreateAccountSuccessful = {
                     navController.popBackStack()
                     navController.navigate(Graph.HOME)
                 },
                 onBackClick = {
                     navController.navigate(AuthScreen.Login.route)
-                }
+                },
+                context = context
             )
         }
     }
